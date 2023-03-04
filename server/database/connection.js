99349b25fbfccbad84052f4694048dd4bc678239
theo-lemoine-oc <propjhh>
2023-03-04 17:15:43 +0100

    create redux reducer and store

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,12 @@
-const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+const mongoose = require('mongoose');
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB';
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Database successfully connected');
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`Database Connectivity Error: ${error}`);
+    throw new Error(error);
   }
 }
