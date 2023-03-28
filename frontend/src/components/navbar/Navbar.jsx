71efc07a17e1335612f1af7a4898e257449b0aef
theo-lemoine-oc <propjhh>
2023-03-28 22:54:06 +0200

    add icons

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux";
 import { useProfile } from '../../hooks/useProfile';
 import { logout } from "../../redux/reducer";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import logo from '../../assets/img/argentBankLogo.png';
 import './navbar.css';
 
@@ -28,12 +31,16 @@ export const Navbar = () => {
             {token && token.length > 0 ? (
                 <div className="user-informations">
                     <p>
+                        <FontAwesomeIcon icon={faCircleUser} className="icon" />
                         { firstName }
                     </p>
-                    <button onClick={ handleSignOut } to="sign-in" className="tab_sign-out">Sign Out</button>
+                    <button onClick={ handleSignOut } to="sign-in" className="tab_sign-out">
+                        <FontAwesomeIcon icon={faRightFromBracket} className="icon" />
+                        Sign Out
+                    </button>
                 </div>
             ) : (
-                <NavLink to="sign-in" className="tab_sign-in">Sign In</NavLink>
+                <NavLink to="sign-in" className="tab_sign-in"><FontAwesomeIcon icon={faCircleUser} className="icon" />Sign In</NavLink>
             )}
         </nav>
     )
